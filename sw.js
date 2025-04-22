2b3f28b505431564c5a2becf3d66b8d23dc15197
Nicolas Fréret <contact.nicolasfreret@gmail.com>
2025-04-22 14:51:15 +0200

    sw2

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
 const CACHE = 'V3'
+const CACHE_IMGS = 'V3_img'
+const CACHE_HTML = 'V3_page'
 
 const CACHE_FILES = [
     '/t/offline.php',
@@ -29,7 +31,7 @@ self.addEventListener('activate', e=>{
            const keys = await caches.keys()
            await Promise.all(
             keys.map(k=>{
-                if( k !== CACHE ) return caches.delete(k)
+                if( k !== CACHE && k !== CACHE_IMGS && k !== CACHE_HTML ) return caches.delete(k)
             })
            )
         })()
@@ -45,20 +47,60 @@ self.addEventListener('fetch', e=>{
            
             if(e.request.destination === 'document'){
                 try{
-                    return await fetch(e.request)
+                    return await cacheFirstThenNetwork(CACHE_HTML, e.request,{
+                        save:true
+                    })
                 }catch (e){
                     const cache = await caches.open(CACHE)
                     return await cache.match('/t/offline.php')
                 }
+            }else if(e.request.destination === 'image'){
+                return await cacheFirstThenNetwork(CACHE_IMGS, e.request,{
+                    save:true
+                })
             }else{
-
-                const cache = await caches.open(CACHE)
-                return await cache.match(e.request) || fetch(e.request)
-
-
+                return await cacheFirstThenNetwork(CACHE, e.request)
             }
                 
         
         })()
     )
 })
+
+//STRATEGIES
+
+async function cacheFirstThenNetwork(_cache, _req, _options = {}){
+
+    const options = {
+        save: false,
+        ..._options
+    }
+
+    const cache = await caches.open(_cache)
+    const cached =  await cache.match(_req)
+    if(cached) return cached
+    const res =  await fetch(_req)
+    if(options.save) await saveInCache(_cache, _req, res)
+    return res
+}
+
+async function cacheOnly(_cache, _req){
+ 
+}
+
+async function networkOnly( _req){
+    return await fetch(_req)
+}
+
+async function networkFirstThen(_cache, _req){
+    
+}
+
+async function saveInCache(_cache, _req, _res){
+
+    const cache = await caches.open(_cache)
+    await cache.put(_req, _res.clone())
+
+    
+}
+
